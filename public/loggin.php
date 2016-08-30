de14f9f2c71ea446cc99d94f0e312e91024987d7
Peychas <Nian_wow@live.se>
2016-08-30 13:53:34 +0200

    loggin sida

---
+++ public/loggin.php
@@ -0,0 +1,34 @@
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/flatly.css">
+</head>
+
+<body>
+	<div class="jumbotron">
+		<div class="container">
+			<h2>Stample</h2>
+		</div>
+	</div>
+	<div class="container">
+		<form method="post" class="">
+			<fieldset>
+				<div class="form-group">
+  					<input class="form-control input-lg" id="focusedInput" type="email" placeholder="Email ex. Stefan.folkesson@it-gymnasiet.se">
+				</div>
+				<div class="form-group">
+					<input class="form-control input-lg" id="focusedInput" type="password" placeholder="Lösenord">
+				</div>
+				<div>
+					<a href="#" class="btn btn-primary btn-lg">Logga In</a>
+				</div>
+			</fieldset>
+		</form>
+	</div>	
+</body>
+
+
+
+<footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</footer>
