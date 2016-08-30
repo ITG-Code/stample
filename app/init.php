39c4a5c91426adb25772196c5656350f92e44612
Hannes Kindströmmer <me@brolaugh.com>
2016-08-30 15:41:36 +0200

    Got exceptions, fixed that

--- app/init.php
+++ app/init.php
@@ -3,9 +3,8 @@
 
 require_once '../app/Core/App.php';
 require_once '../app/Core/Controller.php';
-
-require_once '../app/Core/Database.php';
 require_once '../app/Core/DatabaseConfig.php';
+require_once '../app/Core/Database.php';
 require_once '../app/Helpers/Redirect.php';
 require_once '../app/Helpers/Session.php';
 Session::start();
