ee84ae54d02dc5674ed46f8de12bf4fa10695550
Hannes Kindströmmer <me@brolaugh.com>
2016-08-31 10:26:59 +0200

    Lade till funktionallitet för loggin

--- app/Model/User.php
+++ app/Model/User.php
@@ -6,26 +6,53 @@ class User
     public $email;
     private $password;
     public $fname;
-    public $ename;
+    public $sname;
 
-    public function login(){
+    public function login()
+    {
         $this->email = $_POST['login-email'];
         $this->password = $_POST['login-password'];
 
-        $loginStatus = true;
-
-        if(strlen($this->email) < 1)
-            $loginStatus = false;
-        elseif(strlen($this->password) < 1)
-            $loginStatus = false;
-
-        if($loginStatus){
-            Session::set('email', $this->email);
+        if (password_verify($this->password, $this->createSelfFromEmail($this->email))) {
+            Session::set("sessionUser", $this->id);
             return true;
-        }else
+        } else {
+            Session::delete("sessionUser");
             return false;
+        }
+
     }
-    public function isLoggedIn(){
-        return boolval(Session::get('email'));
+
+    public function isLoggedIn()
+    {
+        return boolval(Session::get("SessionUser")) ? $this->doesIDExist(Session::get('sessionUser')) : false;
+    }
+
+    private function createSelfFromEmail($email)
+    {
+        $stmt = Database::getInstance()->getConnection()->prepare("SELECT * FROM user WHERE email = ?");
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        $stmt->close();
+        $hashedPassword = "";
+        if ($obj = $res->fetch_object()) {
+            $this->id = $obj->id;
+            $this->fname = $obj->fname;
+            $this->sname = $obj->sname;
+            $hashedPassword = $obj->password;
+        }
+        return boolval($hashedPassword) ? $hashedPassword : false;
+    }
+
+    private function doesIDExist($id)
+    {
+        $stmt = Database::getInstance()->getConnection()->prepare("SELECT count(*) as idcount FROM user WHERE id = ?");
+        $stmt->bind_param('i', $id);
+        $stmt->execute();
+        $res = $stmt->get_result();
+        $stmt->close();
+        $row = $res->fetch_object();
+        return boolval($row->idcount);
     }
 }
