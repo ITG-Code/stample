50d4e7fac1f2d21a9305c9629696e3a6c4381e76
Hannes Kindströmmer <me@brolaugh.com>
2016-09-01 11:55:52 +0200

    Satte på php_error

--- app/init.php
+++ app/init.php
@@ -7,5 +7,14 @@ require_once '../app/Core/DatabaseConfig.php';
 require_once '../app/Core/Database.php';
 require_once '../app/Helpers/Redirect.php';
 require_once '../app/Helpers/Session.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+define('INC_ROOT', dirname(__DIR__));
+require_once INC_ROOT.  '/vendor/autoload.php';
+\php_error\reportErrors();
+
 Session::start();
+
 $app = new App;
