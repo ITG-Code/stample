33b136a956da28dcf8ba726a0daf24050fac11a8
Peychas <Nian_wow@live.se>
2016-09-06 13:20:53 +0200

    statistik på en person

---
+++ public/statper.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/public/css/flatly.css">
+	<link rel="stylesheet" type="text/css" href="/public/css/stil.css">
+	<script type="text/javascript" src="/public/js/klocka.js"></script>
+	<title>
+
+	</title>
+</head>
+<body onload="startTid()">
+	<div class="jumbotron">
+		<div class="container">
+			<h2>Namn</h2>
+		</div>
+	</div>
+	<div class="container">
+	<div class="list-group">
+		<div class="row list-group-item">
+			<h3 class="list-group-item-heading text-primary">Denna Veckan</h3>
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+				<div class="well">
+					<p>Mån</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+				<div class="well">
+					<p>Tis</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+				<div class="well">
+					<p>Ons</p>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+				<div class="well">
+					<p>Tor</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+				<div class="well">
+					<p>Fre</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+				<div class="well">
+					<p>Lör</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+				<div class="well">
+					<p class="text-danger">Sön</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+	<div class="container">
+		<div class="list-group">
+		<div class="row list-group-item">
+		<h3 class="list-group-item-heading text-primary">Detta Arbetsåret</h3>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Jan</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Feb</p>
+				</div>	
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Mar</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Apr</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Maj</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Jun</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Jul</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Aug</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Sep</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Okt</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Nov</p>
+				</div>
+			</div>
+			<div class="col-lg-2 col-md-4 col-sm-6 col-xs-12">
+				<div class="well">
+					<p>Dec</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+	</footer>
+</body>
+</html>
