57232480d5a43dde6c9cbfcf2d594a5c42b4e2d8
Hannes Kindströmmer <me@brolaugh.com>
2016-09-12 11:59:30 +0200

    Fixed #15

--- app/Model/Histogram.php
+++ app/Model/Histogram.php
@@ -18,45 +18,22 @@ class Histogram
   public function __construct($user)
   {
     $this->user = $user;
-    $this->getCurrentWeek();
-    $this->getCurrentYear();
-  }
-  private function getCurrentWeek()
-  {
+
     $currentWeeksMonday = date("Y/m/d", strtotime('monday this week'));
     $currentWeeksMonday = new \DateTime($currentWeeksMonday);
-    $startDate = $currentWeeksMonday;
-    $endDate = clone $startDate;
-    $endDate->add(new \DateInterval("P1D"));
-    $stmt = Database::getInstance()->getConnection()->prepare(
-        "SELECT `user`.`id` as userid,checkvalue, COUNT(`check`.`id`) as rows, SUM(UNIX_TIMESTAMP(`check`.stamp)) as times FROM `user`
-LEFT JOIN `check` ON `user`.`id` = `check`.`user`
-WHERE stamp < ? AND `user`.id = ? AND stamp > ?
-GROUP BY `check`.user, `check`.`checkvalue` 
-ORDER BY checkvalue ASC
-");
+    $this->weeks = $this->getPeriod($currentWeeksMonday, new \DateTime(), 'P1D', true);
 
-    while($startDate < new \DateTime()) {
-      $valuedStartDate = $startDate->format("Y-m-d H:i:s");
-      $valuedEndDate = $endDate->format("Y-m-d H:i:s");
-      $stmt->bind_param('sis', $valuedEndDate, $this->user, $valuedStartDate);
-      $stmt->execute();
-      $result = $stmt->get_result();
-      $row1 = $result->fetch_object();
-      $row2 = $result->fetch_object();
-      $this->weeks[] = new HistogramUnit($row1, $row2, $startDate, $endDate, true);
-      $startDate->add(new \DateInterval("P1D"));
-      $endDate->add(new \DateInterval("P1D"));
-    }
-  }
-
-  private function getCurrentYear()
-  {
     $currentYear = new \DateTime();
     $currentYear->setDate($currentYear->format('Y'), 1, 1);
     $currentYear->setTime(0, 0, 0);
-    $endDate = clone $currentYear;
-    $endDate->add(new \DateInterval("P1M"));
+    $this->months = $this->getPeriod($currentYear, new \DateTime(), 'P1M', false);
+  }
+
+  private function getPeriod($startTime, $periodEnd, $interval, $weekOrMonth)
+  {
+
+    $endTime = clone $startTime;
+    $endTime->add(new \DateInterval($interval));
     $stmt = Database::getInstance()->getConnection()->prepare(
         "SELECT `user`.`id` as userid,checkvalue, COUNT(`check`.`id`) as rows, SUM(UNIX_TIMESTAMP(`check`.stamp)) as times FROM `user`
 LEFT JOIN `check` ON `user`.`id` = `check`.`user`
@@ -64,27 +41,30 @@ WHERE stamp < ? AND `user`.id = ? AND stamp > ?
 GROUP BY `check`.user, `check`.`checkvalue` 
 ORDER BY checkvalue ASC
 ");
-
-    while($currentYear < new \DateTime()) {
-      $valuedStartDate = $currentYear->format("Y-m-d H:i:s");
-      $valuedEndDate = $endDate->format("Y-m-d H:i:s");
+    $returnValue = [];
+    while($startTime < $periodEnd) {
+      $valuedStartDate = $startTime->format("Y-m-d H:i:s");
+      $valuedEndDate = $endTime->format("Y-m-d H:i:s");
       $stmt->bind_param('sis', $valuedEndDate, $this->user, $valuedStartDate);
       $stmt->execute();
       $result = $stmt->get_result();
       $row1 = $result->fetch_object();
       $row2 = $result->fetch_object();
-      $this->months[] = new HistogramUnit($row1, $row2, $currentYear, $endDate, false);
-      $currentYear->add(new \DateInterval("P1M"));
-      $endDate->add(new \DateInterval("P1M"));
+      $returnValue[] = new HistogramUnit($row1, $row2, $startTime, $endTime, $weekOrMonth);
+      $startTime->add(new \DateInterval($interval));
+      $endTime->add(new \DateInterval($interval));
     }
+    return $returnValue;
   }
-  public function getViewModel(){
+
+  public function getViewModel()
+  {
     $weekViewModels = [];
-    foreach($this->weeks as $day){
+    foreach($this->weeks as $day) {
       $weekViewModels[] = $day->getViewModel();
     }
     $monthViewModels = [];
-    foreach($this->months as $month){
+    foreach($this->months as $month) {
       $monthViewModels[] = $month->getViewModel();
     }
     return new \Stample\ViewModel\Histogram($this->user, $weekViewModels, $monthViewModels);
