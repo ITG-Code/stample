bb4275b167a503a6c37021b4900b12f526929111
Hannes Kindströmmer <me@brolaugh.com>
2016-09-12 10:55:22 +0200

    Fixade klockan
    Fixed #11

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ function startTid() {
     m = checkTid(m);
     s = checkTid(s);
     document.getElementById('txt').innerHTML = t + ":" + m + ":" + s;
-    var t = setTimeout(startTid, 500);
+    setTimeout(startTid, 500);
 }
 //lägger till en nolla framför tal som är mindre än 10
 function checkTid(i) {
@@ -39,3 +39,5 @@ function setSkin(skin) {
     xhttp.open("GET", "/public/home/setskin/"+skin, true);
     xhttp.send();
 }
+
+startTid();
