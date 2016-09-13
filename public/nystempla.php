3d77c6b714127cd7cb1db038435e0940fd0504fa
Peychas <Nian_wow@live.se>
2016-09-13 09:48:05 +0200

    flyttade knappar och klocka

---
+++ public/nystempla.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" id="theme" href="/public/css/{{ skin }}.css">
+	<link rel="stylesheet" type="text/css" href="/public/css/main.css">
+
+	<title></title>
+</head>
+<body>
+	<div class="jumbotron">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-10 col-md-8 col-sm-8 col-xs-8">
+					<h2>namn</h2>
+				</div>
+				<div class="well col-lg-2 col-md-4 col-sm-4 col-xs-4">
+					<p>Klocka</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-2 ">
+					<a href="" class="btn btn-primary">Stuff</a>
+				</div>
+				<div class="col-lg-1 col-lg-offset-8">
+					<a href="" class="btn btn-danger">Logga Ut</a>
+				</div>
+				<div class="col-lg-1 dropdown">
+					<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><span class="glyphicon glyphicon-cog"></span></button>
+					<ul class="dropdown-menu">
+						<li><a href="">Byt Lösenord</a></li>
+						<li><a href="">Adminpanel</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+				<a href="" class="btn btn-primary btn-lg col-lg-8 col-md-8 col-sm-6 col-xs-6"><h3>Stämpla In</h3></a>
+		</div>
+	</div>
+	<footer>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<script src="/public/js/main.js" charset="utf-8"></script>
+	</footer>
+</body>
+</html>
