1442f648ee7d6b0e040e9498d51482287a7c790e
Peychas <Nian_wow@live.se>
2016-09-13 15:23:59 +0200

    man kan klicka på månader så alla dagar i månaden kommer fram

--- public/nystempla.php
+++ public/nystempla.php
@@ -20,14 +20,14 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-lg-2 ">
+				<div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
 					<a href="" class="btn btn-primary">Stuff</a>
 				</div>
-				<div class="col-lg-1 col-lg-offset-8">
+				<div class="col-lg-1 col-lg-offset-8 col-md-1 col-md-offset-8 col-sm-3 col-sm-offset-6 col-xs-3 col-xs-offset-6">
 					<a href="" class="btn btn-danger">Logga Ut</a>
 				</div>
-				<div class="col-lg-1 dropdown">
-					<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><span class="glyphicon glyphicon-cog"></span></button>
+				<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 dropdown">
+					<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><span class="glyphicon glyphicon-cog responsive"></span></button>
 					<ul class="dropdown-menu">
 						<li><a href="">Byt Lösenord</a></li>
 						<li><a href="">Adminpanel</a></li>
@@ -38,7 +38,88 @@
 	</div>
 	<div class="container">
 		<div class="row">
-				<a href="" class="btn btn-primary btn-lg col-lg-8 col-md-8 col-sm-6 col-xs-6"><h3>Stämpla In</h3></a>
+			<a href="" class="btn btn-primary btn-lg col-lg-8 col-md-8 col-sm-6 col-xs-6"><h3>Stämpla In</h3></a>
+		</div>
+	</div>
+	<ul class="nav nav-tabs" id="statis">
+		<li class="active"><a href="#vecka" data-toggle="tab">Vecka</a></li>
+		<li><a href="#monad" data-toggle="tab">Månad</a></li>
+	</ul>
+	<div id="myTabContent" class="tab-content">
+		<div class="tab-pane fade active in" id="vecka">
+			<div class="container stati">
+				<div class="row text-center">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div class="well">
+							<p></p>
+							<div class="progress">
+								<div class="progress-bar progress-bar-{{ colors[loop.index0] }}"
+								style="width: {{ progress }}%"></div>
+							</div>
+							<div class="progress">
+								<div class="progress-bar" style="width: 0%"></div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="tab-pane fade" id="monad">
+			<div class="container stati">
+				<div class="row text-center">
+					<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<div class="well" data-toggle="collapse" data-target="#Juli">
+							<div>
+								<p>Juni</p>
+								<div class="progress">
+									<div class="progress-bar progress-bar-default"
+									style="width: 40%"></div>
+								</div>
+							</div>
+							<div id="Juli" class="collapse">
+								<div class="row">
+									<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+										<div class="well">
+											<p>Den 1:a</p>
+										</div>
+									</div>
+									<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+										<div class="well">
+											<p>Den 2:a</p>
+										</div>
+									</div>
+									<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+										<div class="well">
+											<p>den tredje</p>
+										</div>
+									</div>
+									<div class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+										<div class="well">
+											<p>den fjärde</p>
+										</div>
+									</div>
+									<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+										<div class="well">
+											<p>den femte</p>
+										</div>
+									</div>
+									<div class="col-lg-4 col-md-4 col-sm-6 col-xs-6">
+										<div class="well">
+											<p>den sjätte</p>
+										</div>
+									</div>
+									<div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+										<div class="well">
+											<p>den sjunde</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 	<footer>
